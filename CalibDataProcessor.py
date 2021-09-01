Overflow in DCS calculation fixed. Works well now.

CalibDataProcessor.py
@@ -45,7 +45,7 @@ class CalibDataProcessor:
             try:
                 os.makedirs(self.output_path / folder)
             except:
-                print(f'Folder "{folder}" already exists!')
+                print(f'\tFolder "{folder}" already exists!')
 
         print('Required folders created')
 
@@ -59,10 +59,16 @@ class CalibDataProcessor:
             # 50 delay line steps, 240 x 320 pixels
             self.raw_data = self.raw_data.reshape((self.n_delay_steps, self.chip_dim[0], self.chip_dim[1]))
 
-        self.mean_vs_dll = np.array([self.raw_data[dll].mean() for dll in range(self.n_delay_steps)])
-        self.stdev_vs_dll = np.array([self.raw_data[dll].std() for dll in range(self.n_delay_steps)])
+            self.calc_mean_std_all_dll()
 
         print('Calibration data loaded,', np.count_nonzero(self.raw_data), 'non-zero elements found.\n')
+
+    def calc_mean_std_all_dll(self):
+        """
+        Calculates mean and std dev for all DL steps.
+        """
+        self.mean_vs_dll = np.array([self.raw_data[dll].mean() for dll in range(self.n_delay_steps)])
+        self.stdev_vs_dll = np.array([self.raw_data[dll].std() for dll in range(self.n_delay_steps)])
 
     def load_fit_params_ext(self):
         """
@@ -101,15 +107,15 @@ class CalibDataProcessor:
             self.raw_data[i] += rollover_shift
 
     @staticmethod
-    def lsb_to_mm(lst, frequency, maxphase=3e4):
+    def lsb_to_mm(lsb, frequency, maxphase=3e4):
         """
         Converts LST units to mm.
-        :param lst: Input LST value to be converted
+        :param lsb: Input LST value to be converted
         :param frequency: Source modulation frequency
         :param maxphase: Max. LSB value
         :return: Distance in mm
         """
-        return 3e8 / frequency / maxphase / 2 * lst * 1000
+        return 3e8 / frequency / maxphase / 2 * lsb * 1000
 
     @staticmethod
     def dll_to_mm(nsteps):
@@ -181,7 +187,7 @@ class CalibDataProcessor:
             plot_title = f'{self.chip}, measurement no. = {delay_step + 1}'
 
         fig, ax = plt.subplots()
-        n, bins, patches = plt.hist(x=self.raw_data[delay_step].reshape((-1)), bins=40, rwidth=.85, color='b')
+        n, bins, patches = plt.hist(x=self.raw_data[delay_step].reshape((-1)), bins=100, rwidth=.85, color='b')
 
         # set labels
         ax.set_xlabel('Distance [mm]')
@@ -591,13 +597,14 @@ class CalibDataProcessor:
 
 
 if __name__ == '__main__':
-    input_file_path = r'C:\Data\01_NFL\NFL_data\calib_data\W438_C269\W438_C269_10000_drnu_images.bin'
+    input_file_path = r'C:\Data\01_NFL\NFL_data\calib_data\W578_C132\W578_C132_10000_drnu_images.bin'
+    # input_file_path = r'C:\Data\01_NFL\NFL_data\calib_data\W438_C269\W438_C269_10000_drnu_images.bin'
     # input_file_path = r'C:\Data\01_NFL\calib_data\W455_C266\W455_C266_10000_drnu_images.bin'
     output_path = r'C:\Data\01_NFL\NFL_data\Analysis\DRNU'
 
     reader = CalibDataProcessor(input_file_path, output_path)
 
-    # reader.create_folders()
+    reader.create_folders()
 
     reader.load_raw_file()
     reader.compensate_rollover()
@@ -606,16 +613,16 @@ if __name__ == '__main__':
     # reader.fit_all_pixels_calib()
 
     # load calibration curve parameters from external file
-    reader.load_fit_params_ext()
+    # reader.load_fit_params_ext()
 
     # reduce number of fit parameters
-    fit_par_red_2 = reader.reduce_fit_params(reader.fit_params_all, [1, 2])
-    fit_par_red_3 = reader.reduce_fit_params(reader.fit_params_all, [1, 2, 4])
+    # fit_par_red_2 = reader.reduce_fit_params(reader.fit_params_all, [1, 2])
+    # fit_par_red_3 = reader.reduce_fit_params(reader.fit_params_all, [1, 2, 4])
 
     # apply calibration on raw data based on the above fit parameters
-    reader.apply_calibration(reader.fit_params_all, True, 'sin_full')  # already started
-    reader.apply_calibration(fit_par_red_2, True, 'sin_red_2')
-    reader.apply_calibration(fit_par_red_3, True, 'sin_red_3')
+    # reader.apply_calibration(reader.fit_params_all, True, 'sin_full')  # already started
+    # reader.apply_calibration(fit_par_red_2, True, 'sin_red_2')
+    # reader.apply_calibration(fit_par_red_3, True, 'sin_red_3')
 
     # load calibrated data from pickle files
     # reader.load_calib_data_ext('sin_full')
@@ -628,26 +635,26 @@ if __name__ == '__main__':
 
     # PLOT THINGS
     # --- Basic plotting
-    # reader.plot_mean_std()
+    reader.plot_mean_std()
     #
     # reader.plot_extreme_pix(3, 5, 'max')
     # reader.plot_extreme_pix(3, 5, 'min')
     #
-    # reader.plot_all('heat')
-    # reader.plot_all('hist')
+    reader.plot_all('heat')
+    reader.plot_all('hist')
     #
-    # reader.create_discr_map(2)
-    # reader.plot_all('discr')
+    reader.create_discr_map(2)
+    reader.plot_all('discr')
     #
-    # reader.create_sigma_map()
-    # reader.plot_sigma_map()
+    reader.create_sigma_map()
+    reader.plot_sigma_map()
 
     # --- Fit required
     # reader.plot_hist_fit_params()
 
     # --- Calibration application required
-    reader.plot_degr(rem_lin_part=True)
-    reader.plot_degr(rem_lin_part=False)
+    # reader.plot_degr(rem_lin_part=True)
+    # reader.plot_degr(rem_lin_part=False)
 
     # ---- TESTING SPACE ----
 

RawDataProcessor.py
@@ -15,18 +15,17 @@ class RawDataProcessor(CalibDataProcessor):
     """
     Reading and processing of raw data.
     """
-    def __init__(self, input_file_path, output_path, n_recorded_frames=25):
+    def __init__(self, input_file_path, output_path):
         super().__init__(input_file_path, output_path)
-        self.n_recorded_frames = n_recorded_frames
         self.raw_frames = np.zeros((1, ))
         self.stdev_pixel = np.zeros((1, ))
 
         self.dcs_frames = np.zeros((1, ))
-        self.mm_from_DCS = np.zeros((1, ))
 
         # set class to single measurement mode
         self.data_type = 'single_meas'
-        self.n_x_steps = self.n_recorded_frames
+        self.n_recorded_frames = 0
+        self.n_x_steps = 0
         self.x_ax_data = range(self.n_x_steps)
 
     def get_file_info(self):
@@ -50,6 +49,9 @@ class RawDataProcessor(CalibDataProcessor):
             self.raw_frames = self.lsb_to_mm(np.array(file['DRNU/distances']), frequency=self.mod_frequency)
             # example: 50 delay line steps, 25 frames, 240 x 320 pixels
 
+        self.n_recorded_frames = self.raw_frames.shape[1]
+        self.n_x_steps = self.n_recorded_frames
+
         print('Raw measured data loaded')
 
     def load_raw_file_DCS(self):
@@ -58,43 +60,42 @@ class RawDataProcessor(CalibDataProcessor):
             - imagesDCS
         """
         with h5py.File(self.input_file_path) as file:
-            self.dcs_frames = np.array(file['DRNU/imagesDCS'])
+            self.dcs_frames = np.array(file['DRNU/imagesDCS'], dtype='float64')
             # example: 50 delay line steps, 25 frames, 240 x 320 pixels, 4 DCS'
+
+        self.n_recorded_frames = self.dcs_frames.shape[1]
+        self.n_x_steps = self.n_recorded_frames
 
         print('Raw measured DCS data loaded')
 
-    def load_mm_from_dcs(self):
+    def load_mm_from_dcs_ext(self):
         """
         Loads distance measurement directly calculated from DCS from pickle
         """
         with open(self.output_path / 'pickl' / f'{self.chip}_mm_from_DCS.pkl', 'rb') as file:
             self.raw_frames = pkl.load(file)
 
+        self.n_recorded_frames = self.raw_frames.shape[1]
+        self.n_x_steps = self.n_recorded_frames
+
         print('Distance measurement directly calculated from DCS loaded from pickle')
 
-    def convert_dcs(self):
+    def convert_dcs_to_mm(self):
         """
         Takes DCS frames, coverts to mm and saves to self.raw_frames
         """
-        print('Converting DCS signals to mm...')
+        print('Converting DCS signals to mm...', end=' ')
 
-        self.mm_from_DCS = np.zeros((self.n_delay_steps, self.n_recorded_frames, self.chip_dim[0], self.chip_dim[1]))
-
-        for dl in range(self.n_delay_steps):
-            print(f'\tDelay line {dl + 1}/{self.n_delay_steps}')
-            for meas in range(self.n_recorded_frames):
-                for pix_x in range(self.chip_dim[0]):
-                    for pix_y in range(self.chip_dim[1]):
-                        self.mm_from_DCS[dl, meas, pix_x, pix_y] = \
-                            RawDataProcessor.dcs_to_mm(self.dcs_frames[dl, meas, pix_x, pix_y, :], self.mod_frequency)
+        data = [self.dcs_frames[:, :, :, :, i] for i in range(4)]
+        self.raw_frames = RawDataProcessor.dcs_to_mm(data, self.mod_frequency)
 
         with open(self.output_path / 'pickl' / (self.chip + '_mm_from_DCS.pkl'), 'wb') as file:
-            pkl.dump(self.mm_from_DCS, file)
+            pkl.dump(self.raw_frames, file)
 
         print('done')
 
     @staticmethod
-    def dcs_to_mm(dcs, frequency=1e6):
+    def dcs_to_mm(dcs, frequency=1e7):
         """
         Formula which converts DCS to mm
         """
@@ -114,6 +115,15 @@ class RawDataProcessor(CalibDataProcessor):
         :param dll: Delay line.
         """
         self.raw_data = self.raw_frames[dll]
+
+    def calc_calib_data(self):
+        """
+        Calculates calibration data out of single frame data by averaging of each measure frame.
+        :return:
+        """
+        print('Calculating calibration data...', end=' ')
+        self.raw_data = np.mean(self.raw_frames, 1)
+        print('done')
 
     def plot_hist_pix_std(self, delay_step):
         """
@@ -146,67 +156,37 @@ if __name__ == '__main__':
         r'C:\Data\01_NFL\NFL_data\raw_data\W578_C132\W578_C132_10000_RawData_DRNU_27082021_170551.hdf5' # 75 frames per DLL, just DCS
         # r'C:\Data\01_NFL\NFL_data\raw_data\W603_C096_41\W603_C096_10000_CalibData_DRNU_09062021_131457.hdf5'
         # r'C:\Data\01_NFL\NFL_data\raw_data\W578_C132\W578_C132_10000_RawData_DRNU_27082021_162711.hdf5' # 50 frames per DLL, just DCS
-    n_frames = 75
     output_path = r'C:\Data\01_NFL\NFL_data\Analysis\Raw_recordings'
 
-    reader = RawDataProcessor(input_file_path, output_path, n_frames)
+    reader = RawDataProcessor(input_file_path, output_path)
 
     reader.create_folders()
     # reader.get_file_info()
-    # reader.load_raw_file()
-    reader.load_raw_file_DCS()
+    # reader.load_raw_file()        # load distance data from h5 file
+    reader.load_raw_file_DCS()    # load DCS data from h5 file
+    reader.convert_dcs_to_mm()
+    # reader.compensate_rollover()
 
-    start = time.time()
-    reader.convert_dcs()
-    print(f'It took {time.time() - start:.2f}')
+    # reader.load_mm_from_dcs_ext()   # load DCS data from pickle file
 
-    # reader.load_mm_from_dcs()
+    reader.calc_calib_data()        # average frames to get calibration data
 
     reader.pick_dll(2)
     reader.plot_all('heat')
     reader.plot_all('hist')
 
-    [reader.plot_hist_pix_std(i) for i in range(reader.n_delay_steps)]
-    #
-    # # plot mean and stdev for all delay lines
-    # start = time.time()
+    # plot mean and stdev as if calibration data was loaded
+    reader.calc_mean_std_all_dll()  # calculate mean and stdev for each DLL
+    reader.data_type = 'dl_step'
+    reader.plot_mean_std()
+    reader.data_type = 'single_meas'
 
-    # Plot mean std
-    for dll in range(reader.n_delay_steps):
-        reader.calc_mean_std(dll)
-        reader.plot_mean_std(file_suff=f'dll-{dll:02d}')
+    # [reader.plot_hist_pix_std(i) for i in range(reader.n_delay_steps)]
 
-    # print(f'It took {time.time() - start:.2f}')
+    # plot mean and stdev for all delay lines
+    # for dll in range(reader.n_delay_steps):
+    #     reader.calc_mean_std(dll)
+    #     reader.plot_mean_std(file_suff=f'dll-{dll:02d}')
 
     # ====== TESTING SPACE =======
-
-    #  THREADING
-    # start = time.time()
-    # threads = []
-    # # for dll in range(reader.n_delay_steps):
-    # for dll in range(2):
-    #     reader.calc_mean_std(dll)
-    #     params = ('single_meas', reader.n_recorded_frames, f'dll-{dll:02d}')
-    #     threads.append(Thread(target=reader.plot_mean_std, args=params))
-    #
-    # [t.start() for t in threads]
-    # [t.join() for t in threads]
-    # print(f'It took {time.time() - start:.2f}')
-    #  ---------------
-
-    # start = time.time()
-
-    # THREADING POOL
-    # with ThreadPoolExecutor() as ex:
-    #     for dll in range(reader.n_delay_steps):
-    #         reader.calc_mean_std(dll)
-    #         ex.submit(reader.plot_mean_std, f'dll-{dll:02d}')
-
-    # PROCESSING POOL
-    # with ProcessPoolExecutor() as ex:
-    #     for dll in range(reader.n_delay_steps):
-    #         reader.calc_mean_std(dll)
-    #         ex.submit(reader.plot_mean_std, f'dll-{dll:02d}')
-    #
-    # print(f'It took {time.time() - start:.2f}')
 